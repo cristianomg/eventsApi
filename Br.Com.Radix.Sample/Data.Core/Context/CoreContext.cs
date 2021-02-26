b9874ca730f24480def41ccfb463a1d349bad6b3
cristiano oliveira <cristianomg95@gmail.com>
2021-02-25 23:54:17 -0300

    adicionado configuração do contexto

---
+++ Br.Com.Radix.Sample/Data.Core/Context/CoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Core.Context
+{
+    public class CoreContext : DbContext
+    {
+        public CoreContext(DbContextOptions<CoreContext> options) : base(options)
+        {
+            this.ChangeTracker.LazyLoadingEnabled = false;
+        }
+        public DbSet<Event> Events { get; set; }
+    }
+}
