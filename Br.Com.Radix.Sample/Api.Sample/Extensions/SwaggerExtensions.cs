cbe5ee268a9eb073d16acadae5a35456746a5e61
cristiano oliveira <cristianomg95@gmail.com>
2021-02-26 00:23:13 -0300

    configuração do swagger

---
+++ Br.Com.Radix.Sample/Api.Sample/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace Api.Sample.Extensions
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Radix",
+                        Version = "v1",
+                        Description = "Api para gerenciamento de eventos sensoriais.",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Cristiano Macedo",
+                            Url = new Uri("https://github.com/cristianomg")
+                        }
+                    });
+            });
+            return services;
+        }
+    }
+}
---
+++ Br.Com.Radix.Sample/Api.Sample/Startup.cs
@@ -0,0 +1,55 @@
+using Api.Sample.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Api.Sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerConfiguration();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sample Api V1");
+                c.RoutePrefix = string.Empty;  // Set Swagger UI at apps root
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
